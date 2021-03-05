faaa957879b8e527a3002ba6c17ac5fb3cb653f6
wass01 <maatallawassim01@gmail.com>
2021-03-05 15:14:44 +0100

    ESERCIZIO TERMINATO

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// creazione ciclo for per ripetere la stampa dei numeri
+for (var i = 1; i <= 100; i++) {
+  // condizioni da rispettare
+  if (i%3 == 0 && i%5 == 0) {
+    console.log("FizzBuzz");
+    document.getElementById('numeri').innerHTML +=
+   "<li>" + "FizzBuzz" + "</li>";
+  } else if (i%3 == 0){
+    console.log("Fizz");
+    document.getElementById('numeri').innerHTML +=
+   "<li>" + "Fizz" + "</li>";
+  } else if (i%5 == 0) {
+    console.log("Buzz");
+    document.getElementById('numeri').innerHTML +=
+   "<li>" + "Buzz" + "</li>";
+  } else {
+    console.log(i);
+    document.getElementById('numeri').innerHTML +=
+   "<li>" + i + "</li>";
+  }
+}
